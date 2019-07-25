a89921aaa979071519390a060eb5509dc048ae9b
GuinivereJane <guinifer.k@gmail.com>
2019-07-25 14:06:42 -0400

    added jwt auth

--- wp-config.php
+++ wp-config.php
@@ -22,6 +22,8 @@ require_once(__DIR__ . '/vendor/autoload.php');
 $dotenv = Dotenv\Dotenv::create(__DIR__);
 $dotenv->load();
 
+define('FS_METHOD', 'direct');
+
 // define('ENVIRONMENT_DEV', 'dev');
 // define('ENVIRONMENT_STAGE', 'stage');
 // define('ENVIRONMENT_PROD', 'prod');
